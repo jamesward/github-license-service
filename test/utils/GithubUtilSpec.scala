6242248024aa98161fd3ebfe9ca45075d2f89b7c
James Ward <james@jamesward.org>
2015-09-11 19:44:39 -0600

    catch poorly spelled LICENCE too

--- test/utils/GithubUtilSpec.scala
+++ test/utils/GithubUtilSpec.scala
@@ -25,7 +25,10 @@ class GithubUtilSpec extends PlaySpec with OneAppPerSuite with ScalaFutures {
     "fail to fetch a license from webjars/webjars" in {
       intercept[Exception] {
         await(githubUtil.license("webjars", "webjars", "master"))
-      }.getMessage must equal ("No LICENSE, LICENSE.md, LICENSE.txt or README.md containing a license found")
+      }.getMessage must equal ("No LICENSE, LICENSE.md, LICENSE.txt, license.txt, licenses.txt or README.md containing a license found")
+    }
+    "fetch a license for n3-charts/line-chart" in {
+      await(githubUtil.license("n3-charts", "line-chart", "master")) must include ("MIT")
     }
   }
 
